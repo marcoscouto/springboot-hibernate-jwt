527baea11483acf3652ef5b75123dcd8723d3c00
Marcos Couto <marcos_otuoc@hotmail.com>
2020-01-31 14:13:29 -0300

    [Creating] Category classes (Domain, Resource)

---
+++ src/main/java/com/marcoscouto/cursomc/resources/CategoryResource.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+
+
+}
