d0ecefcf96b0271b94a4ab223b3117fe9d0df0ab
Marcos Couto <marcos_otuoc@hotmail.com>
2020-02-13 21:46:00 -0300

    Autentication and generate token JWT

---
+++ src/main/java/com/marcoscouto/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.marcoscouto.cursomc.services;
+
+import com.marcoscouto.cursomc.domain.Client;
+import com.marcoscouto.cursomc.repositories.ClientRepository;
+import com.marcoscouto.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Client cli = clientRepository.findByEmail(email);
+        if(cli == null) throw new UsernameNotFoundException(email);
+
+        return new UserSS(
+                cli.getId(),
+                cli.getEmail(),
+                cli.getPassword(),
+                cli.getProfiles());
+    }
+}
